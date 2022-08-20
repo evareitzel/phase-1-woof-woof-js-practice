b4d500bf8ddd34288f84fcead100da4d157135d4
Eva Reitzel <evareitzel@gmail.com>
2022-08-20 19:00:44 -0400

    Challenge 3 - pseudocode,  event listener added

--- src/index.js
+++ src/index.js
@@ -6,18 +6,30 @@
 // Add span w pup name to Dog Bar
 function getPupData() {
   fetch('http://localhost:3000/pups')
-    .then(resp => resp.json())
-    .then((pupData) => {
-      pupData.forEach((pup) => addPupData(pup))
-    })
+  .then(resp => resp.json())
+  // .then((pupData) => console.log(pupData))
+  .then((pupData) => {
+    pupData.forEach((pup) => addPupData(pup))
+  })
 }
 
 function addPupData(pup) {
-  const dogBar = document.querySelector('#dog-bar')
   const dogName = document.createElement('span')
   dogName.innerText = `${pup.name}`
-  console.log(dogName)
+  dogName.addEventListener('click', clickHandler)
   document.querySelector('#dog-bar').append(dogName)
+}
+
+// Challenge 3: Show Info About Each Pup
+  // when user clicks dogName, display info in div#dog-info
+    // img  w URL
+    // h2 w dog's name
+    // button saying 'Good Dog!' or 'Bad Dog!' based on iSGoodDog
+
+function clickHandler(e){
+  console.log(e.target.pup)
+  const dogInfo = document.querySelector('#dog-info')
+  // dogInfo.innerHTML = `<img src='${pup.image}'>`
 }
 
 document.addEventListener('DOMContentLoaded', () => {
